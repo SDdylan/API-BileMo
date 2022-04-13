1a0f051759b67356a67aaa2bce233c727f02e807
SDdylan <38325425+SDdylan@users.noreply.github.com>
2022-04-13 16:00:43 +0200

    Simple serialization of all products now work

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,27 +2,58 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizableInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product", name="app_product")
+     * @Route("/api/product", name="api_product_index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(ProductRepository $productRepository, SerializerInterface $serializer)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+        $products = $productRepository->findAll();
+
+        foreach ($products as $product){
+            $array = [
+                'type' => "phone"
+            ];
+            array_push($product, $array);
+        }
+
+        $json = $serializer->serialize($products, 'json', ['groups' => 'product:read']);
+
+//        foreach($json as $object){
+//            $array = [
+//                'type' => "phone"
+//            ];
+//            $array = json_encode($array);
+//            array_push($object, $array);
+//        }
+
+        $response = new JsonResponse($json, 200, [], true);
+
+        return $response;
     }
+
     /**
      * @Route("/test", name="test")
      */
-    public function test()
+    public function test(ProductRepository $productRepository)
     {
+        $products = $productRepository->findAll();
 
+        $productNormalize = $this->json($products, 200, [], ['groups' => 'product:read']);
+
+        $json = json_encode($productNormalize);
+
+        dd($json);
+
+        return $this->json($productRepository->findAll(), 200, []);
     }
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -6,6 +6,7 @@ use App\Repository\ProductRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
@@ -16,21 +17,25 @@ class Product
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups("product:read")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups("product:read")
      */
     private $model;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups("product:read")
      */
     private $description;
 
     /**
      * @ORM\Column(type="date")
+     * @Groups("product:read")
      */
     private $releaseDate;
 
